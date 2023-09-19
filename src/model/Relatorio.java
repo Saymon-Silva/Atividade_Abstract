0bbf7158bd3004743ad83fbbfdf20e3a77707156
saymonps <saymonsilv401@gmail.com>
2023-09-19 19:54:39 -0300

    acabei

--- src/model/Relatorio.java
+++ src/model/Relatorio.java
@@ -5,4 +5,7 @@ public class Relatorio{
     public String gerarRelatorio(IImprimivel objeto){
         return objeto.mostrarDados();
     }
+    //Não usei em nenhum lugar praticamente, também não sei onde poderia usar.
+    //Ta ai só pra encher linguaça...
+    //Quem sabe futuramente esse metodo ganhe uma implementação em algum lugar.
 }
